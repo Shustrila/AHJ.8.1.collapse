226750f378c878ad54609f96280b0339131cd98f
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-05-13 15:51:52 +0300

    fix readme

--- src/js/Collapse.js
+++ src/js/Collapse.js
@@ -1,56 +1,56 @@
 class Collapse {
-    constructor(button, time = 0.5) {
-        if (typeof time !== 'number') {
-            throw  new Error('time is not number!!!');
-        }
+  constructor(button, time = 0.5) {
+    if (typeof time !== 'number') {
+      throw new Error('time is not number!!!');
+    }
 
-        this.button = button;
-        this.slider = '';
-        this.time = time;
-        this.hiddenClass = 'collapse__slider--hidden';
+    this.button = button;
+    this.slider = '';
+    this.time = time;
+    this.hiddenClass = 'collapse__slider--hidden';
+    this.lock = false;
+
+    this.init();
+  }
+
+  // private:
+  onClickButton() {
+    if (!this.lock) {
+      const hiddenClass = this.slider.classList.contains(this.hiddenClass);
+      const childHeight = this.slider.firstElementChild.clientHeight;
+      const height = childHeight + this.slider.offsetHeight;
+
+      this.slider.classList.toggle(this.hiddenClass);
+      this.slider.style.opacity = 1;
+      this.slider.style.maxHeight = `${height}px`;
+
+      if (!hiddenClass) this.slider.style.maxHeight = '0px';
+
+      setTimeout(() => {
+        if (!hiddenClass) this.slider.style.opacity = 0;
         this.lock = false;
-
-        this.init();
+      }, this.time * 950);
     }
+    this.lock = true;
+  }
 
-// private:
-    onClickButton() {
-        if (!this.lock) {
-            const hiddenClass = this.slider.classList.contains(this.hiddenClass);
-            const childHeight = this.slider.firstElementChild.clientHeight;
-            const height = childHeight + this.slider.offsetHeight;
+  // private:
+  init() {
+    this.button = document.querySelector(this.button);
 
-            this.slider.classList.toggle(this.hiddenClass);
-            this.slider.style.opacity = 1;
-            this.slider.style.maxHeight = height + 'px';
+    this.button.addEventListener('click', this.onClickButton.bind(this));
+  }
 
-            if (!hiddenClass) this.slider.style.maxHeight = '0px';
-
-            setTimeout(() => {
-                if (!hiddenClass) this.slider.style.opacity = 0;
-                this.lock = false;
-            }, this.time * 950)
-        }
-        this.lock = true;
+  // public:
+  slide(slider) {
+    if (typeof slider !== 'string') {
+      throw new Error('slider is not number!!!');
+    } else {
+      this.slider = document.querySelector(slider);
+      this.slider.style.transition = `max-height ${this.time}s ease 0s`;
+      this.slider.style.maxHeight = `${this.slider.offsetHeight}px`;
     }
-
-// private:
-    init() {
-        this.button = document.querySelector(this.button);
-
-        this.button.addEventListener('click', this.onClickButton.bind(this));
-    }
-
-// public:
-    slide(slider) {
-        if (typeof slider !== 'string') {
-            throw  new Error('slider is not number!!!');
-        } else {
-            this.slider = document.querySelector(slider);
-            this.slider.style.transition = `max-height ${this.time}s ease 0s`;
-            this.slider.style.maxHeight =  this.slider.offsetHeight + 'px'
-        }
-    }
+  }
 }
 
 export default Collapse;
